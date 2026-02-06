2f2c6bcb4014d44ece89b253c722d17f4f59fc7e
Thibaut Minard <thibaut.minard@efrei.net>
2026-02-06 11:44:06 +0100

    v2.0.0 - TEST FINAL

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="app">
       <header className="header">
         <h1>🚀 Webhook Demo App</h1>
-        <p className="version">Version 1.3.0 - Live deploy test!</p>
+        <p className="version">Version 2.0.0 - TEST FINAL!</p>
       </header>
       
       <main className="main">
